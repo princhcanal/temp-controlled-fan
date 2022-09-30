02aa29d936b67046a1ed6417601c6d6998a63833
Princh Harold Cañal <princhcanal@gmail.com>
2022-09-30 13:18:02 +0800

    feat(windows-forms): ad initial windows forms code

---
+++ windows-forms/temp-controlled-fan/temp-controlled-fan/Form1.cs
@@ -0,0 +1,15 @@
+namespace temp_controlled_fan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
